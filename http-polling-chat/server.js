754954d507fa64be3813ef8d5836e011c4b8cc6b
gitname <gitname@users.noreply.github.com>
2018-04-04 23:39:31 -0700

    Reference message in request body

--- http-polling-chat/server.js
+++ http-polling-chat/server.js
@@ -31,10 +31,11 @@ app.post('/messages/create', function (req, res) {
     console.log('POST /messages/create', req.body);
 
     // Insert the message into the database if the message string is valid.
+    var message = req.body.message;
     if (validateMessage(message)) {
-        messages.push(req.body.message);
+        messages.push(message);
     }
-    
+
     // Respond with all messages in the database.
     res.send(messages);
 });
@@ -42,6 +43,6 @@ app.post('/messages/create', function (req, res) {
 // Configure the app to listen on some TCP port (number 80 by default).
 //
 var port = process.env.PORT || 80;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('The app is listening on port ' + port + '.');
 });
